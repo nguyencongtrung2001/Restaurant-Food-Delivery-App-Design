d0e07261a1cfdab19ea78e3cc94e6e327e90a082
nguyen cong trung <congtrung1004@gmail.com>
2025-11-12 09:36:59 +0700

    đang chỉnh lỗi hiển thị menu

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,17 +1,39 @@
 import Price from "@/components/Price";
-import { singleProduct } from "@/data";
+import { ProductType } from "@/types/types";
 import Image from "next/image";
 import React from "react";
 
-const SingleProductPage = () => {
+const getData = async (id: string): Promise<ProductType> => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/product/${id}`,
+    {
+      cache: "no-store",
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch product");
+  }
+
+  return res.json();
+};
+
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+const SingleProductPage = async ({ params }: Props) => {
+  const { id } = await params;
+  const product = await getData(id);
+
   return (
     <div className="p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-orange-500 md:flex-row md:gap-8 md:items-center">
       {/* IMAGE CONTAINER */}
-      {singleProduct.img && (
+      {product.img && (
         <div className="relative w-full h-1/2 md:h-[70%]">
           <Image
-            src={singleProduct.img}
-            alt=""
+            src={product.img}
+            alt={product.title}
             className="object-contain"
             fill
           />
@@ -19,9 +41,15 @@ const SingleProductPage = () => {
       )}
       {/* TEXT CONTAINER */}
       <div className="h-1/2 flex flex-col gap-4 md:h-[70%] md:justify-center md:gap-6 xl:gap-8">
-        <h1 className="text-3xl font-bold uppercase xl:text-5xl">{singleProduct.title}</h1>
-        <p>{singleProduct.desc}</p>
-        <Price price={singleProduct.price} id={singleProduct.id} options={singleProduct.options}/>
+        <h1 className="text-3xl font-bold uppercase xl:text-5xl">
+          {product.title}
+        </h1>
+        <p>{product.desc}</p>
+        <Price
+          price={product.price}
+          id={product.id}
+          options={product.options}
+        />
       </div>
     </div>
   );
